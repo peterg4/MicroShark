b65d05ae39815719f082781c3bfbb6d71890d047
peterg4 <43735968+peterg4@users.noreply.github.com>
2021-11-01 12:18:24 -0400

    unkoown backend fixy wixcy

--- routes/api/products.js
+++ routes/api/products.js
@@ -16,11 +16,18 @@ Product = mongoose.model('product', productSchema);
 // @route    GET api/products
 // @desc     check a product for plastics
 // @access   public
+//
+
 router.get('/',  async (req, res) => {
     console.log(req.query.code);
     try {
         const passport = await Product.findOne({code: req.query.code});
-        res.status(200).json(passport);
+        if(passport){
+            res.status(200).json(passport);
+        } else {
+            res.status(200).json({"_id":{"$oid":"0"},"code":"Unknown","hasPlastics":"2"});
+        }
+        
     } catch (err) {
         console.log(err)
         res.status(500).send('Server Error');
